3a038bc0343119d8078cf266b22c6a6bd2e9e8cd
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-02-17 16:40:58 +0200

    adding POST entry point

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fs.readdirSync('./task').forEach(file => {
     let task = JSON.parse(readFileSync(`./task/${file}`, {encoding: 'utf8', flag: 'r'}))
     listOfTasks.push(task)
 });
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/b', (req, res) =>{
     res.send(listOfTasks);
@@ -19,9 +19,11 @@ app.get('/b/:id', (req, res) => {
     res.send(filteredTask);
 })
 
-// app.post('/b', (req, res) =>{
-//     req.writeFileSync('./', req.body);
+app.post('/b', (req, res) =>{
+    const body = JSON.stringify(req.body, null, 4)
+    fs.writeFileSync(`./task/${Date.now()}.json`, body);
+    res.send(body);
 
-// });
+});
 
 app.listen(3000, console.log("listening to port 3000"));
