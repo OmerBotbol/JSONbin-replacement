32fce5f834580106fd4612cb258129686ad93c8c
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-02-17 16:03:14 +0200

    changing logic of the api
    instead of reading from a single JSON file, the api reads all the tasks and create a list of them

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
+const fs= require('fs');
 const {readFileSync} = require('fs');
-const data = JSON.parse(readFileSync("./tasks.json", {encoding: 'utf8', flag: 'r'}));
+
+
+const listOfTasks=[];
+fs.readdirSync('./task').forEach(file => {
+    let task = JSON.parse(readFileSync(`./task/${file}`, {encoding: 'utf8', flag: 'r'}))
+    listOfTasks.push(task)
+});
 // app.use(express.json());
 
 app.get('/b', (req, res) =>{
-    res.send(data)
+    res.send(listOfTasks);
 });
-app.get('/b/:id', (req, res) => {
-    const filteredTask = data.filter(task => task.id === req.params.id);
-    res.send(filteredTask);
-})
+// app.get('/b/:id', (req, res) => {
+//     const filteredTask = data.filter(task => task.id === req.params.id);
+//     res.send(filteredTask);
+// })
+
+// app.post('/b', (req, res) =>{
+//     req.writeFileSync('./', req.body);
+
+// });
 
 app.listen(3000, console.log("listening to port 3000"));
