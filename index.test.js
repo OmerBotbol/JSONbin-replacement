a3e34b4cc42bd32c0d93792747cd35cd9b94176a
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-02-23 19:13:11 +0200

    adding GET fail test

--- index.test.js
+++ index.test.js
@@ -11,10 +11,19 @@ describe("GET entry point", ()=>{
         success: "x"
     };
 
+    const expectedFailMessage = {"message": "Invalid Record ID"}
+
     it("can get a bin by id",async ()=>{
         const response = await request(app).get("/b/1614098060748");
 
         expect(response.status).toBe(200);
         expect(response.body).toEqual(expectedData);
+    })
+
+    test("if an illegal id is requested an appropriate response is sent",async ()=>{
+        const response = await request(app).get("/b/161409806074");
+
+        expect(response.status).toBe(404);
+        expect(response.body).toEqual(expectedFailMessage);
     })
 })
